4ed7ecb5cea2ffaf6c48c2e26a084634f06d20d9
wondwosen-bewketu <wondwossen@etmsoftwareplc.com>
2025-01-29 16:34:07 +0300

    fix: fixthe api client

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://task-managment-backend-0q1f.onrender.com",
+  baseURL: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
--- src/api/apiInterceptors.ts
+++ src/api/apiInterceptors.ts
@@ -1,13 +1,10 @@
-// apiInterceptors.ts
 import apiClient from "./apiClient";
 
-// Optionally, you can add interceptors to handle errors or authorization globally
 apiClient.interceptors.response.use(
-  (response) => response, // Pass through successful responses
+  (response) => response,
   (error) => {
-    // Handle errors globally
     console.error("API Error: ", error);
-    return Promise.reject(error); // Propagate error
+    return Promise.reject(error);
   }
 );
 
